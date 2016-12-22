0e6c93a04a622b4edbd49108707c61671386ce15
Jiahao Li <lijiahao008@gmail.com>
2016-12-22 11:21:44 -0500

    setup board

---
+++ board.rb
@@ -0,0 +1,47 @@
+require_relative "square"
+
+class Board
+  SIZE = 5
+  BOMBS = SIZE * SIZE / 3
+  def initialize(grid = Board.blank_grid )
+    @grid = grid
+  end
+
+  def self.default_grid
+    new_board = self.new
+    new_board.populate
+    new_board
+  end
+
+  def self.blank_grid
+    Array.new(SIZE) { Array.new (SIZE)}
+  end
+
+  def populate
+    num_bombs = BOMBS
+    bombs_pos = []
+    num_bombs.times do |bomb|
+      pos = [rand(grid.length), rand(grid.length)]
+      while bombs_pos.include?(pos)
+        pos = [rand(grid.length), rand(grid.length)]
+      end
+      self[pos] = Square.new(true)
+    end
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    grid[row][col] = val
+  end
+
+  private
+  attr_reader :grid
+end
+
+b = Board.default_grid
+p b
